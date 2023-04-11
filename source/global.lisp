b770c4e99954cdb7008137bf979a4d1656b7b0e2
John Mercouris <john@mercouris.email>
2023-04-11 18:25:02 -0500

    initialize.lisp: track process in order to kill later.

--- source/global.lisp
+++ source/global.lisp
@@ -3,3 +3,4 @@
 (defvar *host* "127.0.0.1")
 (defvar *port* 3000)
 (defvar *script* (uiop:read-file-string "start.js"))
+(defvar *electron-process* nil)
--- source/initialize.lisp
+++ source/initialize.lisp
@@ -1,5 +1,10 @@
 (in-package :cl-electron)
 
-(defun initialize ()
-  (uiop:launch-program (list "electron" "start.js")))
+(defun launch ()
+  (setf *electron-process*
+        (uiop:launch-program (list "electron" "start.js"))))
+
+(defun terminate ()
+  (when (uiop:process-alive-p *electron-process*)
+    (uiop:terminate-process *electron-process*)))
 
