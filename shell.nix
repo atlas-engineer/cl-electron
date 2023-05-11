4d4ae6388171cc449ebc93d0e8dc190806ff556e
Andre A. Gomes <andremegafone@gmail.com>
2023-05-11 22:55:54 +0300

    shell.nix: Minor review.

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
 # SPDX-FileCopyrightText: Atlas Engineer LLC
 # SPDX-License-Identifier: BSD-3-Clause
 
-# This file is meant to be used with the nyxt/gi-gtk system. Use this
+# This file is meant to be used with the cl-electron system. Use this
 # file to open a nix shell with the required dependencies to run SBCL
-# and load nyxt/gi-gtk.
+# and load cl-electron.
 
 { pkgs ? import <nixpkgs> {} } :
 with builtins;
 let inherit (pkgs) stdenv; in
 with pkgs;
 stdenv.mkDerivation {
-  name = "nyxt-dev";
+  name = "cl-electron-dev";
 
   nativeBuildInputs = [
     pkgs.sbcl
