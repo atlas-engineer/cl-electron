946fc85e6e500e6d3f855cd53cacd82a3798b3ea
Andre A. Gomes <andremegafone@gmail.com>
2024-02-02 17:39:55 +0200

    protocol(handle-callback): Handle specifying content-type.

--- source/protocol.lisp
+++ source/protocol.lisp
@@ -21,8 +21,12 @@
 (defmethod handle-callback ((protocol protocol) callback)
   (let ((identifier (new-integer-id)))
     (setf (gethash identifier (callbacks electron:*interface*))
-          (lambda (args) (cl-json:encode-json-to-string `((callback . ,identifier)
-                                                     (result . ,(apply callback args))))))
+          (lambda (args)
+            (cl-json:encode-json-to-string
+             `((callback . ,identifier)
+               ,@(multiple-value-bind (data-string data-type) (apply callback args)
+                   (list `(data-string . ,data-string)
+                         `(data-type . ,(or data-type "text/html;charset=utf8"))))))))
     (handle protocol
             (format nil
 "(request) => {
@@ -32,8 +36,8 @@
             client.on('data', data => {
                 const jsonObject = JSON.parse(data.toString());
                 if (jsonObject.callback == ~a) {
-                   const newResponse = new Response(jsonObject.result, {
-                     headers: { 'content-type': 'text/html' }
+                   const newResponse = new Response(jsonObject.dataString, {
+                     headers: { 'content-type': jsonObject.dataType }
                    });
                    resolve(newResponse);
                 }
