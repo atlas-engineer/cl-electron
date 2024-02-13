78c6d15489281999f55811215aa636c5bb57d9b2
John Mercouris <john@mercouris.email>
2024-02-13 14:12:51 +0200

    protocol: Utilize the new ProtocolSocket class.

--- source/protocol.lisp
+++ source/protocol.lisp
@@ -31,21 +31,12 @@
     return new Promise((resolve, reject) => {
         jsonString = JSON.stringify([ request.url ]);
         ~a.write(`${jsonString}\\n`);
-        let message_buffer = '';
-        ~a.on('data', data => {
-            let data_string = data.toString();
-            let transmission_end_index = data_string.indexOf('');
-            if (transmission_end_index == -1) {
-                message_buffer += data_string;
-            } else {
-                message_buffer += data_string.substring(0, transmission_end_index);
-                const jsonObject = JSON.parse(message_buffer);
-                message_buffer = data_string.substring(transmission_end_index + 1, data_string.length)
-                const newResponse = new Response(jsonObject.dataString, {
-                   headers: { 'content-type': jsonObject.dataType }
-                });
-                resolve(newResponse);
-            }
+        new ProtocolSocket(~a, data => {
+            const jsonObject = JSON.parse(data);
+            const newResponse = new Response(jsonObject.dataString, {
+                headers: { 'content-type': jsonObject.dataType }
+            });
+            resolve(newResponse);
         });
     });
 }" socket-thread-id socket-thread-id))))
