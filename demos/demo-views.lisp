5dc83e28ab80b867182475eb04556a01f478ccde
John Mercouris <john@mercouris.email>
2024-03-04 12:21:20 -0600

    demo-views: Use synchronous JavaScript.
    Note: We load a URL first, otherwise the JavaScript will NEVER finish
    execution. It will block forever. It seems that the webview doesn't
    like to run JavaScript until something has been loaded.

--- demos/demo-views.lisp
+++ demos/demo-views.lisp
@@ -32,13 +32,10 @@
                      (lambda (web-contents)
                        (setf (url main-view) (electron:get-url web-contents))
                        (format t "I have loaded ~a!" (url main-view))))
-  (electron:execute-javascript-with-promise-callback
-   (electron:web-contents main-view)
-   "1 + 1"
-   (lambda (web-contents result)
-     (declare (ignore web-contents))
-     (format t "~%1 + 1 = ~a" result)))
-  (electron:load-url main-view "https://en.wikipedia.org/wiki/Electron"))
+  (electron:load-url main-view "https://en.wikipedia.org/wiki/Electron")
+  (print (electron:execute-javascript-synchronous
+          (electron:web-contents main-view)
+          "1 + 1")))
 
 (define-class modeline (browser-view-bound-to-window) ())
 
