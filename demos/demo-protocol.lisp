f7c770048649c568067928a0569c764f3478e28f
Andre A. Gomes <andremegafone@gmail.com>
2024-03-14 13:12:33 +0200

    demos/demo-protocol: Refactor demo.
    The previous demo demonstrated that setting different scheme privileges results
    in distinctive behavior.  Unfortunately, it no longer works due an Electron bug,
    see below.
    
    https://github.com/electron/electron/issues/41032

--- demos/demo-protocol.lisp
+++ demos/demo-protocol.lisp
@@ -9,18 +9,12 @@
 
         (electron:protocols electron:*interface*)
         (list (make-instance 'electron:protocol
-                             :scheme-name "play"
-                             :privileges "{stream:false}")
-              (make-instance 'electron:protocol
-                             :scheme-name "noplay"
-                             :privileges "{stream:true}")))
+                             :scheme-name "test"
+                             :privileges "{}")))
   (electron:launch electron:*interface*)
-  (electron:handle (find "noplay" (electron:protocols electron:*interface*)
-                         :key #'electron:scheme-name :test #'string-equal)
-                   "() => {return net.fetch('https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3')}")
-  (electron:handle (find "play" (electron:protocols electron:*interface*)
-                         :key #'electron:scheme-name :test #'string-equal)
-                   "() => {return net.fetch('https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3')}")
+  (electron:handle (find "test" (electron:protocols electron:*interface*)
+                           :key #'electron:scheme-name :test #'string-equal)
+                   "() => {return new Response('Hello test scheme.')}")
   (let ((win (make-instance 'electron:browser-window :options "{frame: false}"))
         (view1 (make-instance 'electron:browser-view))
         (view2 (make-instance 'electron:browser-view)))
@@ -28,5 +22,5 @@
     (electron:add-browser-view win view2)
     (electron:set-bounds view1 0 0 400 200)
     (electron:set-bounds view2 0 200 400 200)
-    (electron:load-url view1 "play:dummy-var")
-    (electron:load-url view2 "noplay:dummy-var")))
+    (electron:load-url view1 "test:dummy-var")
+    (electron:load-url view2 "test:dummy-var")))
