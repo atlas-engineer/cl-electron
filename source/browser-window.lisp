46bc53ca935674659080132e43e7cfc7eb1debf2
John Mercouris <john@mercouris.email>
2024-03-25 13:35:59 -0500

    browser-window: Autohide menu bar by default.

--- source/browser-window.lisp
+++ source/browser-window.lisp
@@ -6,7 +6,7 @@
 (in-package :electron)
 
 (defmethod initialize-instance :after ((browser-window browser-window)
-                                       &key (options "{frame: true}"))
+                                       &key (options "{frame: true, autoHideMenuBar: true}"))
   (send-message-interface
    (interface browser-window)
    (format nil "~a = new BrowserWindow(~a);"
