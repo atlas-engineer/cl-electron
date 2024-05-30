2c828deaffb5e97ec177303accf333c6839927ae
Andre A. Gomes <andremegafone@gmail.com>
2024-05-30 12:15:46 +0300

    protocol: Fix indentation.

--- source/protocol.lisp
+++ source/protocol.lisp
@@ -21,13 +21,13 @@
   (let ((socket-thread-id
           (create-node-socket-thread
            (lambda (response)
-            (cl-json:encode-json-to-string
-             (multiple-value-bind (data-string data-type) (apply callback response)
-               (list (cons "dataString" data-string)
-                     (cons "dataType" (or data-type "text/html;charset=utf8")))))))))
+             (cl-json:encode-json-to-string
+              (multiple-value-bind (data-string data-type) (apply callback response)
+                (list (cons "dataString" data-string)
+                      (cons "dataType" (or data-type "text/html;charset=utf8")))))))))
     (handle protocol
             (format nil
- "(request) => {
+"(request) => {
     return new Promise((resolve, reject) => {
         jsonString = JSON.stringify([ request.url ]);
         ~a.write(`${jsonString}\\n`);
