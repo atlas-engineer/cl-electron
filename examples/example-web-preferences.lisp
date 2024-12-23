2d800d1bca249cac64210e9f7990c7b3cfaf7a57
John Mercouris <john@mercouris.email>
2024-12-23 15:13:36 -0600

    example-web-preferences: Use a web-preferences object.

--- examples/example-web-preferences.lisp
+++ examples/example-web-preferences.lisp
@@ -22,6 +22,8 @@
   (setf electron:*interface* (make-instance 'electron:interface))
   (electron:launch)
   ;; Note: WebPreferences can only be set during object creation!
-  (let ((win (make-instance 'electron:window)))
-    (make-instance 'example-view :window win :options "{webPreferences: {images: false}}")
+  (let ((win (make-instance 'electron:window))
+        (preferences (make-instance 'electron:web-preferences)))
+    (electron:add-preference preferences "images" nil)
+    (make-instance 'example-view :window win :web-preferences preferences)
     win))
