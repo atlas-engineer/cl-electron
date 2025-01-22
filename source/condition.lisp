68537a6f6a37769981eb16cf9a884395e2b5cf4e
Andre A. Gomes <andremegafone@gmail.com>
2025-01-22 22:13:41 +0200

    condition: Export javascript-renderer-error.

--- source/condition.lisp
+++ source/condition.lisp
@@ -3,6 +3,7 @@
 
 (in-package :electron)
 
+(export-always 'javascript-renderer-error)
 (define-condition javascript-renderer-error (error)
   ((code :initarg :code :initform nil :reader code))
   (:documentation "A condition for JavaScript errors within the renderer view."))
